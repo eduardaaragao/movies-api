3a84544574841aaa7bbe160dea6e9c4f62a747bb
eduardaaragao <eduardaaragao18@gmail.com>
2022-02-11 23:31:38 +0000

    express-generator typescript

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,13 @@
-import express, { Application, Request, Response } from "express";
+import './pre-start'; // Must be the first import
+import logger from 'jet-logger';
+import server from './server';
 
-const app: Application = express();
-const port = 3000;
 
-// Body parsing Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// Constants
+const serverStartMsg = 'Express server started on port: ',
+        port = (process.env.PORT || 3000);
 
-app.get(
-    "/",
-    async (_req: Request, res: Response): Promise<Response> => {
-        return res.status(200).send({
-            message: "Hello World!",
-        });
-    }
-);
-
-try {
-    app.listen(port, (): void => {
-        console.log(`Successfully connected at port http://localhost:${port}`);
-    });
-} catch (error) {
-    console.error(`Error occured`);
-}
+// Start server
+server.listen(port, () => {
+    logger.info(serverStartMsg + port);
+});
